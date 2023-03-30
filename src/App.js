31485a04b70038b9419dec66be8f2281b8a73c09
shoyo <senku_snap@protonmail.com>
2023-03-30 18:12:29 +0530

    added globalcss files and also imported them in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import "./globalStyles/reset.css";
+import "./globalStyles/variables.css";
+import "./globalStyles/default.css";
+import "./globalStyles/utility.css";
+import Header from "./components/Header";
+
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
