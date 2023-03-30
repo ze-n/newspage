73786a5d6962cc711c317e96c497263da5564cff
shoyo <senku_snap@protonmail.com>
2023-03-30 18:11:46 +0530

    finished Header component work

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Header.css";
+import { ReactComponent as Logo } from "../assets/icons/logo.svg";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="container header__container">
+        <Logo className="header__logo" />
+        <nav className="header__nav">
+          <ul className="header__nav-list" role="list">
+            <li className="header__nav-items">
+              <a href="#" className="header__nav-links">
+                Home
+              </a>
+            </li>
+            <li className="header__nav-items">
+              <a href="#" className="header__nav-links">
+                New
+              </a>
+            </li>
+            <li className="header__nav-items">
+              <a href="#" className="header__nav-links">
+                Popular
+              </a>
+            </li>
+            <li className="header__nav-items">
+              <a href="#" className="header__nav-links">
+                Trending
+              </a>
+            </li>
+            <li className="header__nav-items">
+              <a href="#" className="header__nav-links">
+                Catagories
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
